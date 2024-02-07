77470803810d631443b653ac699c2393061d3426
seohyeonwook <skut01541@gmail.com>
2024-02-07 13:56:11 +0900

    ddd

--- todolist/js/modal.js
+++ todolist/js/modal.js
@@ -1,3 +1,42 @@
+//window.onload = () => {
+    // const day =["일","월","화","수","목","금","토"];
+    // let now = new Date ();
+    // console.log(now.getFullYear());
+    // console.log(now.getMonth()+ 1);//자바스크립트는 0월(==1월)부터시작한다
+    // console.log(now.getDate());
+    // console.log(day[now.getDay()]);
+    //-------------------------------------------------------------------
+    // let arr = new Array();
+    // let obj = {
+    //     id: 1,
+    //     name: "김준일"
+    // }
+    // let obj2 = {
+    //     id: 2,
+    //     name: "김준이"
+    // }
+
+    // arr.push(obj);
+    // arr.push(obj2);
+
+    // console.log(arr);
+
+    // console.log(JSON.stringify(arr));// 객체를 ->json stringify == json형태로 바꿔준다
+    // console.log(JSON.stringify(obj)); //json 자바스크립트에서 사용되는 표준 객체형태
+    // console.log(JSON.stringify(obj2));
+
+    // let jsonArr = JSON.stringify(arr);
+
+    // console.log(JSON.parse(jsonArr))//parse = json 을 다시 객체로
+//}
+
+window.onkeyup = (e) => { //window = 전체
+   if(e.keyCode === 27) {
+     handleCancelClick();
+   }
+}
+
+
 function handleAddTodoModalOpen() {
     const modal = document.querySelector(".root-modal");// document전체 
     const title = modal.querySelector(".modal-title");
@@ -10,7 +49,60 @@ function handleAddTodoModalOpen() {
     modal.classList.add("modal-show");
 }
 
+function handleEditTodoModalOpen(todoId) {
+    const modal = document.querySelector(".root-modal");
+    const title = modal.querySelector(".modal-title");
+    const todoInput = modal.querySelector(".todo-input");
+    const submitButton = modal.querySelector(".modal-button");
+    title.innerHTML="수정하기";
+
+    let todoListJson = localStorage.getItem("todoList");
+    let todoList = todoListJson != null ? JSON.parse(todoListJson) : new Array();
+    
+    let findTodoByTodoId = todoList.filter(todo => todo.todoId === todoId);
+
+
+    todoInput.value = findTodoByTodoId.content;
+    submitButton.onclick = handleEditTodoSubmit;
+
+    modal.classList.add("modal-show");
+}
+
+function convertDateKor(curruntDate) {//date라는 변수를 받아준다.
+    const dayKors =["일","월","화","수","목","금","토"];
+    const year = curruntDate.getFullYear();
+    const month = curruntDate.getMonth() + 1;
+    const date = curruntDate.getDate();
+    const day = dayKors[curruntDate.getDay()];
+    return `${year}년 ${month}월 ${date}일 (${day})`;
+}
+
 function handleAddTodoSubmit() {
+    const modal = document.querySelector(".root-modal");
+    const todoInput = document.querySelector(".todo-input");
+    modal.classList.remove("modal-show");
+    //저장 시작----------------------------
+    let todoListJson = localStorage.getItem("todoList");
+    //todoList = localStorage의 key값 에있는 value값꺼내오겠다
+    let todoList = todoListJson != null ? JSON.parse(todoListJson) : new Array();
+
+    let lastTodoId = todoList.length === 0 ? 0 : todoList[todoList.length - 1].todoId;
+    
+
+    let todoObject = {
+        todoId: lastTodoId + 1,
+        content: todoInput.value,
+        date: convertDateKor(new Date())
+    }
+
+    todoList.push(todoObject);
+    
+    localStorage.setItem("todoList",JSON.stringify(todoList));
+    //저장 완 ---------------------------
+    getTodoList();
+}
+
+function handleEditTodoSubmit() {
     const modal = document.querySelector(".root-modal");
     modal.classList.remove("modal-show");
 }
---
+++ todolist/js/todo.js
@@ -0,0 +1,60 @@
+Window.onload = () => {
+    getTodoList();
+
+    let arr = [1,2,3,4,5,6,7,8,9,10];
+    console.log(arr);
+
+    let newArr = [];
+
+    for(let i = 0; i <arr.length; i++) {
+        if(arr[i] % 2 ===0 ) {
+            newArr.push(arr[i]);
+        }
+    }
+
+    console.log(newArr);
+
+    let newArr2 = arr.filter(num => num % 2 ===0);
+    console.log(newArr2);
+
+}
+
+function getTodoList() {
+    const todoContentList = document.querySelector(".todo-content-list");
+
+    const todoListJson = localStorage.getItem("todoList");
+    const todoList = todoListJson !== null ? JSON.parse(todoListJson) : new Array();
+
+    todoContentList.innerHTML = "";//반복이 돌기전에 비운다는 뜻
+
+    for (let todo of todoList) {
+        todoContentList.innerHTML += `
+            <li class="todo-content-box">
+                        <div class="todo-content-header">
+                            <span>
+                                <i class="fa-regular fa-star"></i>
+                            </span>
+                            <span class="todo-content-date">
+                                ${todo.date}
+                            </span>
+                        </div>
+
+                        <div class="todo-content-main">
+                            <pre class="todo-content"> ${todo.content}
+                            </pre>
+                        </div>
+
+                        <div class="todo-content-footer">
+                            <button class="todo-edit-button" onclick="handleEditTodoModalOpen(${todo.todoId})">
+                                <i class="fa-solid fa-pencil"></i>
+                                <!--fontawesome  -->
+                            </button>
+                            <button class="todo-remove-button">
+                                <i class="fa-regular fa-trash-can"></i>
+                            </button>
+                        </div>
+            </li>
+        `;
+
+    }
+}
